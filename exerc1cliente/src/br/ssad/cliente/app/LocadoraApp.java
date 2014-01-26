68663392edec844c450664604fedd87187973336
paulovf <paulovfrancisco@gmail.com>
2014-01-26 17:51:17 -0200

    alterado: remoção de métodos desnecessaŕios e correções nas rotinas de cadastro e exclusão

--- exerc1cliente/src/br/ssad/cliente/app/LocadoraApp.java
+++ exerc1cliente/src/br/ssad/cliente/app/LocadoraApp.java
@@ -1,25 +1,9 @@
 package br.ssad.cliente.app;
 
-
-
 public class LocadoraApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Menu().criaMenu();
-		/*try{
-			InitialContext contexto = new InitialContext();
-			Locadora locadora = (Locadora)contexto.lookup("LocadoraBean/remote");
-			Filme filme = new Filme();
-			filme.setNome("teste");
-			filme.setValorLocacao(8);
-			filme.setGenero("drama");
-			filme.setDuracao(120);
-			filme.setAnoLancamento(1699);
-			locadora.cadastrarFilme(filme, "bene");
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
 	}
 }
